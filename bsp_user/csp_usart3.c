54796711346a80ceabe52e320189b15099e33dec
lijianfengsoft <li_jian__feng@163.com>
2018-04-13 11:00:56 +0800

    csp ping self is success,but kiss has bugs

---
+++ bsp_user/csp_usart3.c
@@ -0,0 +1,164 @@
+﻿#include "stm32f4xx.h"
+#include "stm32f4xx_gpio.h"
+#include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
+#include "misc.h"
+
+#include "FreeRTOS.h"
+#include "semphr.h"
+#include "queue.h"
+#include "task.h"
+
+#include "csp.h"
+#include "csp_debug.h"
+#include "csp_usart.h"
+#include "csp_thread.h"
+#include "usart.h"
+
+#define MAX_RECEIVE 235
+#define PORT 10
+
+static xQueueHandle USART_REC;
+static usart_callback_t usart_callback = NULL;
+static void usart3_rx_task(void *vptr_args);
+
+void csp_usart3_init(uint32_t bound)
+{
+	GPIO_InitTypeDef	GPIO_InitStructure;
+	USART_InitTypeDef	USART_InitStructure;
+	NVIC_InitTypeDef	NVIC_InitStructure;
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3, ENABLE);
+
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource10, GPIO_AF_USART3);
+	GPIO_PinAFConfig(GPIOB, GPIO_PinSource11, GPIO_AF_USART3);
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10 | GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+	USART_InitStructure.USART_BaudRate = bound;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+	USART_Init(USART3, &USART_InitStructure);
+
+	USART_Cmd(USART3, ENABLE);
+
+	USART_REC = xQueueCreate(MAX_RECEIVE, sizeof(uint8_t));
+	if (USART_REC == NULL)
+		return;
+
+	portBASE_TYPE ret = xTaskCreate(usart3_rx_task, "usart3_rx_task", configMINIMAL_STACK_SIZE * 10, NULL, tskIDLE_PRIORITY + 6, NULL);
+	if (ret != pdTRUE)
+		csp_log_error("Failed to start usart router task\r\n");
+
+	USART_ITConfig(USART3, USART_IT_RXNE, ENABLE);
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+	NVIC_InitStructure.NVIC_IRQChannel = USART3_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 5;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+}
+
+void usart3_putc(char c) {
+	/* 发送一个字节数据到USART1 */
+	USART_SendData(USART3, (uint8_t)c);
+
+	/* 等待发送完毕 */
+	while (USART_GetFlagStatus(USART3, USART_FLAG_TC) == RESET);
+}
+
+void usart_set_callback(usart_callback_t callback)
+{
+	usart_callback = callback;
+}
+
+void usart_insert(char c, void * pxTaskWoken)
+{
+	if (pxTaskWoken == NULL)
+		xQueueSendToBack(USART_REC, &c, 0);
+	else
+		xQueueSendToBackFromISR(USART_REC, &c, pxTaskWoken);
+}
+
+static void usart3_rx_task(void *vptr_args)
+{
+	unsigned int length, count;
+	static uint8_t * cbuf = NULL;
+	uint8_t *ptr = NULL;
+
+	while (1) {
+
+		length = uxQueueMessagesWaiting(USART_REC);
+		ptr = cbuf;
+		count = length;
+		if ((cbuf = (uint8_t *)pvPortMalloc(1000)) == NULL)
+		{
+			printf("malloc failed\r\n");
+		}
+
+		//printf("length is:%u\r\n",length);
+		while (count--)
+		{
+			xQueueReceive(USART_REC, ptr++, 0);
+			//printf( "%c\r\n", *ptr ); 
+		}
+		if (usart_callback&&length)
+			//printf("excute call_back,%d  //\r\n",length);
+			usart_callback(cbuf, (unsigned int)length, (void *)NULL);
+
+		//	my_usart_rx(cbuf,length,NULL);
+		vPortFree(cbuf);
+
+		vTaskDelay(100);
+	}
+}
+
+void USART3_IRQHandler(void)
+{
+	uint8_t ch;
+
+	if (USART_GetITStatus(USART3, USART_IT_RXNE) != RESET)
+	{
+
+		ch = USART_ReceiveData(USART3);
+		//printf( "%c", ch );    //将接受到的数据直接返回打印
+		xQueueSendFromISR(USART_REC, &ch, 0);
+	}
+}
+
+
+CSP_DEFINE_TASK(task_client)
+{
+	char *outbuf = "a-hello world";
+	char inbuf[100] = { 0 };
+	int result;
+	int node = 1;
+
+	while (1)
+	{
+
+		result = csp_ping(node, 100, 100, CSP_O_NONE);
+			printf("Client: Ping %d node result %d [ms]\r\n",node, result);
+
+			if (result!=-1)
+			{
+				outbuf = "a-hello world";
+				csp_transaction(0, node, 10, 1000, outbuf, strlen(outbuf), inbuf, 2);
+				printf("10 -Quit response from server: %s\r\n", inbuf);
+				memset(inbuf, 0, sizeof(inbuf));
+			}
+			csp_sleep_ms(2000);
+	}
+
+	return CSP_TASK_RETURN;
+}
---
+++ main.c
@@ -0,0 +1,149 @@
+﻿/**************************************************
+ *  功能:main 函数
+ *  日期:2018/4/13 10:54
+ *  输入:
+ *  输出:
+ *  注意点:版本1
+ *************************************************/
+#include "stm32f4xx.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "usart.h"
+#include "console.h"
+#include "command.h"
+#include "bsp_i2c_task.h"
+#include "csp_thread.h"
+#include "csp_usart.h"
+
+
+#define START_TASK_PRIO  1					//开始任务优先级
+#define START_STK_SIZE  128					//开始任务堆栈
+TaskHandle_t StartTask_Handler;             //开始任务句柄
+void start_task(void *pvParameters);		//开始任务实体函数声明
+
+#define DEBUG_TASK_PRIO  4					//调试任务优先级
+#define DEBUG_STK_SIZE  130*4				//调试任务堆栈
+TaskHandle_t DebugTask_Handler;             //调试任务句柄
+
+#define I2CSLAVE_TASK_PRIO  2					//I2C从机任务优先级
+#define I2CSLAVE_STK_SIZE  130*4				//I2C从机任务堆栈
+TaskHandle_t I2CslaveTask_Handler;             //I2C从机任务句柄
+
+#define I2CMASTER_TASK_PRIO  2					//I2C主机任务优先级
+#define I2CMASTER_STK_SIZE  130*4				//I2C主机任务堆栈
+TaskHandle_t I2CmasterTask_Handler;             //I2C主机任务句柄
+
+#define CSP_TASK_CLINET_PRIO 3
+#define CLINER_STK_SIZE		130*10
+csp_thread_handle_t	handle_client;
+
+
+CSP_DEFINE_TASK(task_server) {
+
+	csp_socket_t *socket = csp_socket(CSP_SO_NONE);
+	csp_conn_t *conn;
+	csp_packet_t *packet;
+	csp_packet_t *response;
+
+
+
+	csp_bind(socket, CSP_ANY);
+	csp_listen(socket, 5);
+
+	printf("Server task started\r\n");
+
+	while (1) {
+		if ((conn = csp_accept(socket, 100)) == NULL) {
+			continue;
+		}
+
+		while ((packet = csp_read(conn, 100)) != NULL) {
+			switch (csp_conn_dport(conn)) {
+			case 10:
+				printf("10 port is ok");
+				response = csp_buffer_get(sizeof(csp_packet_t) + 2);
+				if (response == NULL) {
+					return CSP_TASK_RETURN;
+				}
+				response->data[0] = 'O';
+				response->data[1] = 'K';
+				response->length = 2;
+				csp_buffer_free(packet);
+				csp_send(conn, response, 1000);
+				break;
+			default:
+				csp_service_handler(conn, packet);
+				break;
+			}
+		}
+
+		csp_close(conn);
+	}
+
+	csp_buffer_free(response);
+
+	return CSP_TASK_RETURN;
+}
+
+int main()
+{
+		bsp_init();
+
+		xTaskCreate((TaskFunction_t)start_task,           
+			        (const char*   )"start_task",         
+			        (uint16_t      )START_STK_SIZE,       
+			        (void*         )NULL,                  
+			        (UBaseType_t   )START_TASK_PRIO, 
+			        (TaskHandle_t* )&StartTask_Handler); 
+		vTaskStartScheduler();	
+		
+		while (1);
+
+		return 0;
+}
+
+void start_task(void *pvParameters)
+{
+	taskENTER_CRITICAL();
+
+	xTaskCreate((TaskFunction_t)debug_console,
+				(const char*	)"debug_task",
+				(uint16_t		)DEBUG_STK_SIZE,
+				(void*			)NULL,
+				(UBaseType_t	)DEBUG_TASK_PRIO,
+				(TaskHandle_t*	)&DebugTask_Handler);
+/*
+	xTaskCreate((TaskFunction_t)i2c_master_task,
+				(const char*	)"i2c_master_task",
+				(uint16_t		)I2CMASTER_STK_SIZE,
+				(void*			)NULL,
+				(UBaseType_t	)I2CMASTER_TASK_PRIO,
+				(TaskHandle_t*	)&I2CmasterTask_Handler);
+
+	xTaskCreate((TaskFunction_t)i2c_slave_task,
+				(const char*	)"i2c_slave_task",
+				(uint16_t		)I2CSLAVE_STK_SIZE,
+				(void*			)NULL,
+				(UBaseType_t	)I2CSLAVE_TASK_PRIO,
+				(TaskHandle_t*	)&I2CslaveTask_Handler);
+		*/				
+	csp_thread_create((TaskFunction_t			)task_client, 
+					  (const signed char * const)"CLINET", 
+					  (unsigned short			)CLINER_STK_SIZE, 
+	  				  (void *					)NULL, 
+					  (unsigned int				)CSP_TASK_CLINET_PRIO, 
+					  (csp_thread_handle_t *	)&handle_client);
+	  
+	csp_thread_create((TaskFunction_t			)task_server,
+					 (const signed char * const)"CLINET",
+					 (unsigned short			)CLINER_STK_SIZE,
+					 (void *					)NULL,
+					 (unsigned int				)CSP_TASK_CLINET_PRIO,
+					 (csp_thread_handle_t *		)&handle_client);
+		
+	vTaskDelete(StartTask_Handler);
+	taskEXIT_CRITICAL();
+}
+
+
+
